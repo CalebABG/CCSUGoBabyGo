a0646832702b37af089dfc384bd66d9d61d3d6c5
Caleb A. BG <flameoff@yahoo.com>
2019-04-29 01:09:11 -0400

    Added FontAwesome5 properly. Removed a ton of img files from Android and iOS project because FontAwesome will be taking over once again. With having it installed properly this time, the issue of icons appearing on Android and not on iOS should be fixed. Updated App.xaml, Info.plist and SensorCalibrationPage.xaml accordingly.

--- GoBabyGoV2/GoBabyGoV2.Android/Resources/Resource.designer.cs
+++ GoBabyGoV2/GoBabyGoV2.Android/Resources/Resource.designer.cs
@@ -4447,26 +4447,26 @@ namespace GoBabyGoV2.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020135
-			public const int avd_hide_password_1 = 2130837813;
+			// aapt resource value: 0x7f020149
+			public const int avd_hide_password_1 = 2130837833;
 			
-			// aapt resource value: 0x7f020136
-			public const int avd_hide_password_2 = 2130837814;
+			// aapt resource value: 0x7f02014a
+			public const int avd_hide_password_2 = 2130837834;
 			
-			// aapt resource value: 0x7f020137
-			public const int avd_hide_password_3 = 2130837815;
+			// aapt resource value: 0x7f02014b
+			public const int avd_hide_password_3 = 2130837835;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020138
-			public const int avd_show_password_1 = 2130837816;
+			// aapt resource value: 0x7f02014c
+			public const int avd_show_password_1 = 2130837836;
 			
-			// aapt resource value: 0x7f020139
-			public const int avd_show_password_2 = 2130837817;
+			// aapt resource value: 0x7f02014d
+			public const int avd_show_password_2 = 2130837837;
 			
-			// aapt resource value: 0x7f02013a
-			public const int avd_show_password_3 = 2130837818;
+			// aapt resource value: 0x7f02014e
+			public const int avd_show_password_3 = 2130837838;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -5107,11 +5107,11 @@ namespace GoBabyGoV2.Droid
 			// aapt resource value: 0x7f020129
 			public const int notification_icon_background = 2130837801;
 			
-			// aapt resource value: 0x7f020133
-			public const int notification_template_icon_bg = 2130837811;
+			// aapt resource value: 0x7f020147
+			public const int notification_template_icon_bg = 2130837831;
 			
-			// aapt resource value: 0x7f020134
-			public const int notification_template_icon_low_bg = 2130837812;
+			// aapt resource value: 0x7f020148
+			public const int notification_template_icon_low_bg = 2130837832;
 			
 			// aapt resource value: 0x7f02012a
 			public const int notification_tile_bg = 2130837802;
@@ -5120,25 +5120,85 @@ namespace GoBabyGoV2.Droid
 			public const int notify_panel_notification_icon_bg = 2130837803;
 			
 			// aapt resource value: 0x7f02012c
-			public const int round_sync_24 = 2130837804;
+			public const int round_360_24 = 2130837804;
 			
 			// aapt resource value: 0x7f02012d
-			public const int round_sync_black_18 = 2130837805;
+			public const int round_360_white_18 = 2130837805;
 			
 			// aapt resource value: 0x7f02012e
-			public const int round_sync_black_24 = 2130837806;
+			public const int round_360_white_24 = 2130837806;
 			
 			// aapt resource value: 0x7f02012f
-			public const int round_sync_black_36 = 2130837807;
+			public const int round_360_white_36 = 2130837807;
 			
 			// aapt resource value: 0x7f020130
-			public const int round_sync_black_48 = 2130837808;
+			public const int round_360_white_48 = 2130837808;
 			
 			// aapt resource value: 0x7f020131
-			public const int tooltip_frame_dark = 2130837809;
+			public const int round_power_settings_new_24 = 2130837809;
 			
 			// aapt resource value: 0x7f020132
-			public const int tooltip_frame_light = 2130837810;
+			public const int round_power_settings_new_black_18 = 2130837810;
+			
+			// aapt resource value: 0x7f020133
+			public const int round_power_settings_new_black_24 = 2130837811;
+			
+			// aapt resource value: 0x7f020134
+			public const int round_power_settings_new_black_36 = 2130837812;
+			
+			// aapt resource value: 0x7f020135
+			public const int round_power_settings_new_black_48 = 2130837813;
+			
+			// aapt resource value: 0x7f020136
+			public const int round_report_24 = 2130837814;
+			
+			// aapt resource value: 0x7f020137
+			public const int round_report_black_18 = 2130837815;
+			
+			// aapt resource value: 0x7f020138
+			public const int round_report_black_24 = 2130837816;
+			
+			// aapt resource value: 0x7f020139
+			public const int round_report_black_36 = 2130837817;
+			
+			// aapt resource value: 0x7f02013a
+			public const int round_report_black_48 = 2130837818;
+			
+			// aapt resource value: 0x7f02013b
+			public const int round_security_24 = 2130837819;
+			
+			// aapt resource value: 0x7f02013c
+			public const int round_security_black_18 = 2130837820;
+			
+			// aapt resource value: 0x7f02013d
+			public const int round_security_black_24 = 2130837821;
+			
+			// aapt resource value: 0x7f02013e
+			public const int round_security_black_36 = 2130837822;
+			
+			// aapt resource value: 0x7f02013f
+			public const int round_security_black_48 = 2130837823;
+			
+			// aapt resource value: 0x7f020140
+			public const int round_sync_24 = 2130837824;
+			
+			// aapt resource value: 0x7f020141
+			public const int round_sync_black_18 = 2130837825;
+			
+			// aapt resource value: 0x7f020142
+			public const int round_sync_black_24 = 2130837826;
+			
+			// aapt resource value: 0x7f020143
+			public const int round_sync_black_36 = 2130837827;
+			
+			// aapt resource value: 0x7f020144
+			public const int round_sync_black_48 = 2130837828;
+			
+			// aapt resource value: 0x7f020145
+			public const int tooltip_frame_dark = 2130837829;
+			
+			// aapt resource value: 0x7f020146
+			public const int tooltip_frame_light = 2130837830;
 			
 			static Drawable()
 			{
